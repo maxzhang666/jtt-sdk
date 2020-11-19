901b00b579263f47a713d5f1aeac4e5d0eef3a64
Max zhang <373884384@qq.com>
2020-11-19 17:14:04 +0800

    热搜词

---
+++ src/Request/Search/HotSearchRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace MaxZhang\JttSdk\Request\Search;
+
+
+use MaxZhang\JttSdk\JttRequest;
+
+/**
+ * 热搜词
+ * Class HotSearchRequest
+ * @package MaxZhang\JttSdk\Request\Search
+ */
+class HotSearchRequest extends JttRequest
+{
+
+    protected $apiMethodName = 'hot_search';
+
+
+    /**
+     * 生成参数数组
+     * @return array
+     */
+    function generateParams()
+    {
+        return [];
+    }
+
+    /**
+     * 参数验证
+     * @return void
+     */
+    function check()
+    {
+
+    }
+}
