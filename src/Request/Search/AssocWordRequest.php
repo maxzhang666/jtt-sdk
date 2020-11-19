a6ba647b42dc963c1fc324eaca2ea34d10cfc640
Max zhang <373884384@qq.com>
2020-11-19 17:10:48 +0800

    联想词

---
+++ src/Request/Search/AssocWordRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace MaxZhang\JttSdk\Request\Search;
+
+
+use MaxZhang\JttSdk\Exceptions\InvalidArgumentException;
+use MaxZhang\JttSdk\JttRequest;
+
+/**
+ * Class AssocWordRequest
+ * @package MaxZhang\JttSdk\Request\Search
+ */
+class AssocWordRequest extends JttRequest
+{
+    protected $apiMethodName = 'assoc_word';
+
+
+    /**
+     * 关键词
+     * @var string
+     */
+    public $word;
+
+    /**
+     * 生成参数数组
+     * @return array
+     */
+    function generateParams()
+    {
+        return ['word' => $this->word];
+    }
+
+    /**
+     * 参数验证
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    function check()
+    {
+        if (empty($this->word)) {
+            throw new InvalidArgumentException('word is required!');
+        }
+    }
+}
