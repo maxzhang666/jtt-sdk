86e7f6c246a0ca4aee7106e04a0596e4fbf4aa03
Max zhang <373884384@qq.com>
2021-04-26 17:07:34 +0800

    商品详情版本更新

--- src/Request/Update/GoodsInfoRequest.php
+++ src/Request/Update/GoodsInfoRequest.php
@@ -14,7 +14,7 @@ use MaxZhang\JttSdk\JttRequest;
 class GoodsInfoRequest extends JttRequest
 {
     protected $apiMethodName = 'get_goods_info';
-    protected $version = 'v2';
+    protected $version = 'v3';
     /**
      *
      * @var string 商品sku搜索，最高可输入100个，多sku搜索时请用英文逗号分割 （注：1、v1版本支持传商品链接；2、如果输入的sku串中某个skuID的商品不在推广中[就是没有佣金]，返回结果会自动过滤该商品）
